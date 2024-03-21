2e3a5e8321e1578e9afe6c794f10c18cb7e92787
sanagareddy <53176627+sanagareddy@users.noreply.github.com>
2024-03-21 17:53:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "oci_identity_compartment" "ociCpt" {
+ compartment_id = "ocid1.compartment.oc1..aaaaaaaayhiyzvzjkl2zapsxhtjgzz3yrou7ceypdfjscwygmnamorbsuzpq"
  name = "ociLearning"
  description= "This is from terraform for learning"
 }
