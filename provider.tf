10c5d8e6b89780f3de2b4b738646f52292a54023
sanagareddy <53176627+sanagareddy@users.noreply.github.com>
2024-03-21 18:17:07 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,4 @@
 provider "oci" {
  region = "US Midwest"
  auth = "InstancePrincipal"
- #user_ocid = "ocid1.user.oc1..aaaaaaaa2sclsgmpfs5xhswws5yyu7dibccc5wjkudgwbl4c6owxtr5cklfa"
- #identity_url     = "https://identity.us-ashburn-1.oraclecloud.com"
 }
